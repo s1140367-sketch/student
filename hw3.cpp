a1cfc3f61028fc6cab9a188d51dc39b110eaf172
s1140367-sketch <s1140367@dmhs.kh.edu.tw>
2025-11-07 11:56:14 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+   
+    std::cout<<"台股大跌" << std :: endl;
+    std::cout<<"台 股 大 跌" << std :: endl;
+    std::cout<<"台\t 股\t 大\t 跌" << std :: endl;
+    std::cout<<"台\n 股\n 大\n 跌" << std :: endl;
+    std::cout<<"\"台股大跌\"" << std :: endl;
+    std::cout<<"台股大跌" << std :: endl;
+    std::cout<<"\\台股大跌\\" << std :: endl;
+  
+}
