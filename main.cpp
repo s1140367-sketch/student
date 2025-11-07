47cb381ec60401c7fff4297cf6802a8bafcbd320
s1140367-sketch <s1140367@dmhs.kh.edu.tw>
2025-11-07 11:40:50 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout <<"heiio, world!" << std::endl;
-    return 0;
+int main()
+{
+   
+    std::cout<<"台股大跌" << std :: endl;
+    std::cout<<"台 股 大 跌" << std :: endl;
+    std::cout<<"台\t 股\t 大\t 跌" << std :: endl;
+    std::cout<<"台\n 股\n 大\n 跌" << std :: endl;
+    std::cout<<"\"台股大跌\"" << std :: endl;
+    std::cout<<"台股大跌" << std :: endl;
+    std::cout<<"\\台股大跌\\" << std :: endl;
+  
 }
