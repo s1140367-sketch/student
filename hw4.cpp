b50525daa297eacf68159dd45a388e40d987d4fd
s1140367-sketch <s1140367@dmhs.kh.edu.tw>
2025-11-17 16:08:36 +0800

    Add files via upload

---
+++ hw4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a = 5 + 3 ;
+    int b = 5 - 3 ;
+    int c = 5 * 3 ;
+    int d = 5 / 3 ;
+    double e = 5 / 3.0 ;
+    int f = 5 % 3 ;
+    
+    std ::cout << a <<""<< b <<""<< c <<""<< d <<""<< e <<""<< f <<""
+    ;return 0;
+}
