e7a1ea56b1673ad091cf73f0663d69cbaba5d3a6
s1140367-sketch <s1140367@dmhs.kh.edu.tw>
2025-12-15 16:12:10 +0800

    Add files via upload

---
+++ hw13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    int score[5][3] = {
+        {80, 90, 85},
+        {70, 75, 78},
+        {88, 92, 95},
+        {60, 65, 70},
+        {90, 93, 89}
+    };
+    
+    string name[5] = {
+        "Amy",
+        "Ben",
+        "Cindy",
+        "David",
+        "Eva"
+    };
+    for (int i=0; i<5; i++) {
+        int sum=0;
+        cout << name[i] <<" 的成績: ";
+        for (int j=0;j<3;j++) {
+            cout << score[i][j] << " ";
+            sum = sum + score[i][j];
+        }
+        cout<< "總分為:" << sum << endl;    
+    }
+    return 0;
+}
